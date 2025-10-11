5223776d91fa5991051c98b2a9cd0c15e1aa4b92
SuryaPratap <suryapratap592006@gmail.com>
2025-10-11 20:49:03 +0530

    Update Herosection.jsx

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,9 +1,17 @@
-import React from "react"
+"use client";
+
+import React from "react";
 
 function Herosection() {
+
   return (
-    <section className="flex flex-col items-center justify-center text-center mt-45">
-      <div className="max-w-3xl">
+    <div className="relative min-h-screen overflow-hidden">
+      <section className="relative z-10 flex flex-col items-center justify-center text-center px-4 py-32">
+        <video className="absolute background-video" style={{top:0, width:'100%', filter:'blur(5px) brightness(0.5)', zIndex:'-1',}} autoPlay loop muted playsInline>
+          <source src="/videos/bg1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="max-w-3xl z-2 mt-30">
         <h1 className="text-4xl md:text-6xl font-bold tracking-wide bg-gradient-to-r from-purple-600 via-pink-500 to-yellow-400 bg-clip-text text-transparent drop-shadow-[0_0_10px_rgba(70,70,70,0.6)]">
           Blockchain Based
           <br />
@@ -19,26 +27,27 @@ function Herosection() {
 
         <div className="mt-10 flex flex-wrap justify-center gap-4">
           <a
-            href="#"
+            href="/complaints/new"
             className="px-7 py-3 rounded-full  bg-gray-200/90 text-black font-semibold shadow-md hover:scale-105 transition-transform"
           >
             File a Complaint
           </a>
           <a
-            href="#"
+            href="/analytics"
             className="px-7 py-3 rounded-full border border-gray-500 text-gray-200 hover:border-white hover:text-white transition"
           >
             Track Progress
           </a>
           <a
-            href="#"
+            href="/contact"
             className="px-7 py-3 rounded-full bg-white/10 border border-gray-600 text-gray-200 hover:bg-white/20 transition"
           >
             Contact Support
           </a>
         </div>
       </div>
-    </section>
+      </section>
+    </div>
   )
 }
 

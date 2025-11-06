271f321abc1f3df0d407b23afcdba3a0c59d9411
Ishaan Aggrawal <ishaanaggrawal101@gmail.com>
2025-11-06 22:18:15 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- Rag-chatbot-and-classifier/fastapi_app.py
+++ Rag-chatbot-and-classifier/fastapi_app.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
